b1f0e097367ab6d46fec0d627beacb5e0c01ee58
Манечка <marusiavanukova95@mail.ru>
2021-12-18 20:16:00 +0300

    game code

--- test.py
+++ test.py
@@ -4,9 +4,6 @@ import mock
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
         @mock.patch('game.birdr')
         @mock.patch('game.can_score')
         def test_check(self, birdr, can_score):
@@ -15,7 +12,13 @@ class MyTestCase(unittest.TestCase):
             birdr.center =(600,100)
             tubes = [game.tubesu.get_rect(midtop=(600, 100))]
             self.assertFalse(game.check(tubes))
-            self.assertTrue(can_score)
+            self.assertTrue(game.can_score)
+
+        def test_check2(self):
+            self.assertEqual(game.new_score(0,1),1)
+            self.assertEqual(game.new_score(1, 0), 1)
+            self.assertEqual(game.new_score(2,0),2)
+            self.assertEqual(game.new_score(0,5),5)
 
 if __name__ == '__main__':
     unittest.main()
